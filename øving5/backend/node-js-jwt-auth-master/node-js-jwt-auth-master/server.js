09d11fdb106e4afcac539f4d8ca20bfb7889b3eb
trygveam <trygveam@stud.ntnu.no>
2022-03-26 17:53:28 +0100

    jada

--- øving5/backend/node-js-jwt-auth-master/node-js-jwt-auth-master/server.js
+++ øving5/backend/node-js-jwt-auth-master/node-js-jwt-auth-master/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081",
 };
 
 app.use(cors(corsOptions));
@@ -20,6 +20,7 @@ const db = require("./app/models");
 const Role = db.role;
 
 db.sequelize.sync();
+initial();
 // force: true will drop the table if it already exists
 // db.sequelize.sync({force: true}).then(() => {
 //   console.log('Drop and Resync Database with { force: true }');
@@ -32,8 +33,8 @@ app.get("/", (req, res) => {
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -44,16 +45,16 @@ app.listen(PORT, () => {
 function initial() {
   Role.create({
     id: 1,
-    name: "user"
+    name: "user",
   });
- 
+
   Role.create({
     id: 2,
-    name: "moderator"
+    name: "moderator",
   });
- 
+
   Role.create({
     id: 3,
-    name: "admin"
+    name: "admin",
   });
 }
